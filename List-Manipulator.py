f9cbbafc60dbcc411aae6c77bb6750e89ffe6f92
110Hadi <hadi1raza1807@gmail.com>
2024-08-04 19:37:01 -0700

    Added The add_item Function

--- List-Manipulator.py
+++ List-Manipulator.py
@@ -24,3 +24,45 @@ def create_list(list_name): ##Function which creates a new list with the name of
         raise KeyError('List of name "', list_name, '" already exists')
     else:
         my_lists[list_name] = []
+
+
+
+def add_item(list_name, item, priority):
+    '''
+    Adds items to a specified list.
+
+    Adds item to the list list_name at a given priority.
+
+    Parameters::
+
+    list_name(str): Name of the list to which an item is to be added.
+    item(str): The name of item which is to be added to the list.
+    priority(int): The position in the list where an item is added.
+
+    Raises:
+    ValueError: If priority is less than 1.
+    KeyError: If the name of list does not already exists.
+    '''
+
+    
+    if priority < 1:
+        raise ValueError('A Positive Number Is Required')
+
+    if not list_name in my_lists:
+        raise KeyError('List of name "', list_name, '" does not exist.')
+    else:
+        if priority >= len(my_lists[list_name]):
+            item = f'{len(my_lists[list_name]) + 1} {item}'
+            my_lists[list_name].append(item)
+
+        else:
+            item = f'{priority} {item}'
+            my_lists[list_name].insert(priority - 1, item)
+
+            for items in range(priority, len(my_lists[list_name])):
+                current_item = my_lists[list_name][items]
+                index = current_item.find(' ')
+                old_position = current_item[:index]
+                new_position = int(old_position) + 1
+                current_item = f'{new_position}{current_item[index:]}'
+                my_lists[list_name][items] = current_item
